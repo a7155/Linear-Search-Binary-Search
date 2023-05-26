2345627948c7c0dde8644a3fdcf82f03b6f842b8
a7155 <114379365+a7155@users.noreply.github.com>
2023-05-26 11:14:42 +0700

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,89 @@
+
+/**
+ *
+ * @author 4715
+ */
+import java.util.Arrays;
+
+public class Search {
+    public static void main(String[] args) {
+        int[] arr = {33, -1, 0, 89, 9, 62, 8, 2, 97};
+        int key = 33;
+
+        System.out.println("Unsorted Array: " + Arrays.toString(arr));
+
+        // Bubble sort
+        long startTime = System.nanoTime();
+        bubbleSort(arr);
+        long endTime = System.nanoTime();
+        long bubbleSortTime = endTime - startTime;
+
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+
+        // Binary search
+        startTime = System.nanoTime();
+        int binaryIndex = binarySearch(arr, key);
+        endTime = System.nanoTime();
+        long binarySearchTime = endTime - startTime;
+
+        if (binaryIndex == -1) {
+            System.out.println("Element " + key + " not found");
+        } else {
+            System.out.println("Element " + key + " found at index: " + binaryIndex);
+        }
+
+        // Linear search
+        startTime = System.nanoTime();
+        int linearIndex = linearSearch(arr, key);
+        endTime = System.nanoTime();
+        long linearSearchTime = endTime - startTime;
+
+        if (linearIndex == -1) {
+            System.out.println("Element " + key + " not found");
+        } else {
+            System.out.println("Element " + key + " found at index: " + linearIndex);
+        }
+
+        System.out.println("Bubble Sort Time: " + bubbleSortTime + " nanoseconds");
+        System.out.println("Binary Search Time: " + binarySearchTime + " nanoseconds");
+        System.out.println("Linear Search Time: " + linearSearchTime + " nanoseconds");
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static int binarySearch(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
